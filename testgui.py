a19510c3fc43b356267d206a9705787809acca65
pmarichal1 <45908725+pmarichal1@users.noreply.github.com>
2024-05-29 22:41:52 -0400

    added more labels

--- testgui.py
+++ testgui.py
@@ -53,21 +53,19 @@ def opt():
 
 # Generates plots where applicable
 def runfunc(func):
-    #myUI.pbar.start(interval=175)
+    myUI.pbar.start(interval=175)
     func()
-    #myUI.pbar.stop()
+    myUI.pbar.stop()
 
     for file in os.listdir():
         if 'plot' in file:
             newwin = tk.Toplevel(myUI)
             newwin.configure(bg='white')
-            #newwin.iconphoto(False, myUI.icon)
+            newwin.iconphoto(False, myUI.icon)
 
-            #im = tk.PhotoImage(file=file)
-            #plot = tk.Label(newwin, image=im, bg='white')
-            plot = tk.Label(newwin, bg='white')
-
-            #plot.image = im
+            im = tk.PhotoImage(file=file)
+            plot = tk.Label(newwin, image=im, bg='white')
+            plot.image = im
             plot.grid(row=0, column=0)
 
             os.remove(file)
@@ -100,23 +98,21 @@ def plotog():
 class LabUIApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        labUI = tk.Frame(self)
-        #labUI.configure(bg='white')
-        #labUI.title('Baxter Lab GUI V2.1.0')
-        #labUI.pack(side="top", fill="x")
-        #self.icon = tk.PhotoImage(file='Stowaway.png')
-        #labUI.iconphoto(False, icon)
+        self.configure(bg='white')
+        self.title('Baxter Lab GUI V2.1.0')
+        #self.canvas = tk.Canvas(self, height=900, width=800, bg='#ffa700')
+        self.icon = tk.PhotoImage(file='Stowaway.png')
+        self.iconphoto(False, self.icon)
+        self.photo = tk.PhotoImage(file='BaxterLogo.png')
+        self.photo = self.photo.subsample(3, 3)
+        self.logo = tk.Label(self, image=self.photo, bg='white')
+        self.logo.image = self.photo
+        self.logo.grid(row=0, column=0, columnspan=4)
 
-        #self.photo = tk.PhotoImage(file='BaxterLogo.png')
-        #self.photo = photo.subsample(3, 3)
-        #self.logo = tk.Label(labUI, image=photo, bg='white')
-        #self.logo.image = photo
-        #self.logo.grid(row=0, column=0, columnspan=4)
-
-        #pbar = Progressbar(labUI, length=50, mode='determinate')
-        #pbar.grid(row=1, column=0, columnspan=4, pady=5, sticky='ew') 
+        self.pbar = Progressbar(self, length=50, mode='determinate')
+        self.pbar.grid(row=1, column=0, columnspan=4, pady=5, sticky='ew') 
         self.ferm_brand = tk.StringVar()
-        self.ferm_brand.set("ALL")   # default value
+        self.ferm_brand.set("IPA")   # default value
         self.abv_brand = tk.StringVar()
         self.abv_brand.set("IPA")    # default value
         self.report_batch = tk.StringVar()
@@ -129,24 +125,29 @@ class LabUIApp(tk.Tk):
         self.plot_points.set('100')  
         #b1 = tk.Button(self, text="print to stdout", command=self.print_stdout).grid(column=0, row=0)
         #b2 = tk.Button(self, text="print to stderr", command=self.print_stderr).grid(column=0, row=1)
-        resetb = tk.Button(self, text='Reset', command=reset,font=('calibri', 12), 
+        tk.Button(self, text='Reset', command=reset,font=('calibri', 12), 
             bg='red', relief='groove', width=12).grid(sticky = 'w',column=0, row=2)
 
         # # Plotting Standard Fermentation Curve
-        fmcurveLB1 = tk.Label(self, text='Fermentation Curve', font=('calibri', 12), 
+        tk.Label(self, text='Fermentation Curve', font=('calibri', 12), 
             bg='lightblue', relief='groove').grid(sticky='w', column=0, row=3)
-        fmcurveLB2 = tk.Label(self, text='Brand Code: ', font=('calibri', 12), 
+        tk.Label(self, text='Brand Code: ', font=('calibri', 12), 
             bg='gray', relief='groove', width=20,justify='left').grid(sticky='w',column=1, row=3)
-        #fmentry = tk.Entry(self, textvariable=ferm_brand).grid(column=3, row=3)
-        fmbutton = tk.Button(self, text='Plot', command=plotferm,bg='green').grid(column=4, row=3)
-        #window.pack()
+        tk.Entry(self, textvariable=self.ferm_brand).grid(column=3, row=3)
+        tk.Button(self, text='Plot', command=plotferm,bg='green').grid(column=4, row=3)
+        
+#  create new frame that will contain output text frame with scrollbar
+        #self.frame2 = tk.Frame(self, highlightbackground="black", highlightcolor="black", highlightthickness=4, bg='#2c2f33')
+        tk.Label(self, text='Output ', font=('calibri', 12),
+            bg='white', relief='groove', height=20,width=40,justify='left').grid(sticky='w',column=0, row=11,columnspan=4)
+        # #  create a Scrollbar and associate it with txt
+        #self.scrollb2 = tk.Scrollbar(self)(column=2,row=10)
+        #self.scrollb2.pack(side='right', fill='y')
 
-        #self.text = tk.Text(self, wrap="word").grid(column=0, row=10)
-        #self.text.pack(side="top", fill="both", expand=True)
-        #self.text.tag_configure("stderr", foreground="#b22222")
-
-        #sys.stdout = TextRedirector(self.text, "stdout")
-        #sys.stderr = TextRedirector(self.text, "stderr")
+        # #  create a Text widget
+        #self.txt2 = tk.Text(self, font=("Calibri", 12), borderwidth=3, wrap='word', undo=True,
+        #                 yscrollcommand=self.scrollb2.set).grid(sticky='w',column=0, row=11)
+        # self.scrollb2.config(command=self.txt2.yview)
 
     def print_stdout(self):
         '''Illustrate that using 'print' writes to stdout'''
@@ -168,5 +169,6 @@ class TextRedirector(object):
         self.widget.configure(state="disabled")
 
 myUI = LabUIApp()
-print('class', myUI.ferm_brand)
+#  redirecting output from script to Tkinter Text window
+#sys.stdout = Std_redirector(myUI.txt2)
 myUI.mainloop()
